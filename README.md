f602d87f14ba2ffeb5e819e7b343a2da0cb3cc80
AlmirPaulo <ap.freelas@gmail.com>
2020-11-24 00:32:15 -0300

    first things

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Wiki_Scraper
-A software to scrap wikipedia articles in PDF files easy and quickly, built in Python with a GUI. 
+A software to scrap wikipedia articles in ".txt" files easy and quickly, built in Python with a GUI. 
---
+++ wiki_scrapper.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import requests, lxml
+#import PySimpleGUI as sg
+
+#1 URL to scrap
+
+#2 Check in "robots.txt" if it's ok to scrap this URL.
+
+#3 Write the text in a pdf file. 
+with open('note_test.txt', 'w') as f:
+    f.write('testando')
+    f.close()
