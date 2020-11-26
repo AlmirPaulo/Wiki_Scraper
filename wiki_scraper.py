464a7d88fa3b03ada65a668beaed3bad9c3776e0
AlmirPaulo <ap.freelas@gmail.com>
2020-11-25 23:34:56 -0300

    Terminal version

--- wiki_scraper.py
+++ wiki_scraper.py
@@ -7,7 +7,7 @@ import requests, lxml, re, time
 #Variables
 print('Please set the Article you want to scrap. Paste just the url after the /wiki/')
 url = input('>')
-print('In wich language this article is written?')
+print('In wich language this article is written? Paste the language code (e.g.: en, pt, fr, es...)')
 lang = input('>')
 root = 'https://'+lang+'.wikipedia.org/wiki/'+url
 
@@ -20,17 +20,19 @@ url_ban = re.search(r'Spe.ial:', url)
 url_ban2 = re.search(r'Spe.ial%3A', url)
 #Check
 if url_ban != None:
-    print("I'm sorry, this page is blocked for scrapings")
+    print("Sorry, this page is blocked for scrapings")
 elif url_ban2 != None:
-    print("I'm sorry, this page is blocked for scrapings")
+    print("Sorry, this page is blocked for scrapings")
 else:
 #Scrap
     print('Just a moment please...')
     time.sleep(10)
     page = requests.get(root).text
     soup = BeautifulSoup(page, 'lxml')
-#3 Write the text in a .txt file. 
-    with open('wikiscrap.txt', 'w') as f:
-        f.write()
-        f.close()
+    tag = soup.find_all('p')
+    for i in tag:
+        #3 Write the text in a .txt file.
+        with open('note_wikiscrap.txt', 'a') as f:
+            f.write(i.text)
+            f.close()
     print('Done! Go check your files.')
